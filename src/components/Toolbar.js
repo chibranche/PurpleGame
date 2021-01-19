e76772cff65efcca7105dec32a12f82c7a25e449
JulienMorinFS <julien.morin@finance-security.eu>
2021-01-19 22:53:36 +0100

    First commit - initialize

---
+++ src/components/Toolbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import './Toolbar.css'
+
+export class Toolbar extends Component {
+    render() {
+        return (
+            <div className="toolbar-container">
+                TOOLBAR
+            </div>
+        )
+    }
+}
+
+export default Toolbar
